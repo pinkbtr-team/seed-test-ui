23442c6abcb5712e00a5e4e070d637685ec9a177
Alexander Mazaletskiy <alexander.mazaletskiy@gmail.com>
2023-04-26 12:50:17 +0200

    Updates

---
+++ static/js/5.1ba320a5.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppincubator=this.webpackJsonppincubator||[]).push([[5],{352:function(p,n){}}]);
+//# sourceMappingURL=5.1ba320a5.chunk.js.map
--- static/js/5.52641be6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonppincubator=this.webpackJsonppincubator||[]).push([[5],{438:function(p,n){}}]);
-//# sourceMappingURL=5.52641be6.chunk.js.map
